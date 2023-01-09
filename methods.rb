356b0e83145daf289e1bdacd344ea7cda86e705a
ZhiLeong <saber_zhileong@hotmail.com>
2023-01-09 11:21:55 -0500

    commit tree 37441671fa782b2ff0e2c8f22f8e9c7896af6cbc

--- methods.rb
+++ methods.rb
@@ -1,1 +1,27 @@
-# Your code here!
+def greet_programmer
+   puts "Hello, programmer!" 
+ 
+end
+
+def greet(name = "Naureen" "Jimmy")
+    puts "Hello, #{name}!"
+
+end
+
+def greet_with_default  (name = "programmer")
+    puts "Hello, #{name}!"
+
+end
+    
+def add (num1, num2)
+    return num1 + num2
+end
+
+def halve (num1)
+    if num1.class != Integer
+        return nil
+    end
+
+    num1 / 2
+
+end
